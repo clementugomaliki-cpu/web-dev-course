c1696367d217429d43e27a8d051b7b15b34709a5
Clement Maliki <clementugomaliki@gmail.com>
2026-08-02 13:49:10 +0100

    swap nodemailer for resend email service

--- Backend/Express/MerchByLucius/controllers/creatormail.js
+++ Backend/Express/MerchByLucius/controllers/creatormail.js
@@ -1,6 +1,9 @@
 const Creator = require("../models/creators");
-const nodemailer = require("nodemailer");
+//const nodemailer = require("nodemailer");
+const {Resend} = require("resend");
 require("dotenv").config();
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendVerificationEmail(email){
    const registeredCreator = await Creator.findOne({email});
@@ -13,28 +16,42 @@ async function sendVerificationEmail(email){
    await registeredCreator.save();
  
    //console.log("SMTP config:", process.env.SMTP_HOST, process.env.SMTP_PORT, process.env.EMAIL);
-   const transporter = await nodemailer.createTransport({
-      service: "gmail",
-      // host: process.env.SMTP_HOST,
-      // port: process.env.SMTP_PORT,
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSW
-    }
-   });
-   const newMail = {
-      from: `"Merch by Lucius" <${process.env.EMAIL}>`,
+   // const transporter = await nodemailer.createTransport({
+   //    service: "gmail",
+   //    // host: process.env.SMTP_HOST,
+   //    // port: process.env.SMTP_PORT,
+   //    auth: {
+   //      user: process.env.EMAIL,
+   //      pass: process.env.PASSW
+   //  }
+   // });
+   try {
+      const {data, error} = await resend.emails.send({
+        
+      from: `Merch by Lucius <onboarding@resend.dev>`,
       to: email,
       subject: "Your verification code",
       html: `<p>Your verification code is <strong>${otp}</strong>. It expires in 15 minutes.</p>`
-   };
-   try {
-      const info = await transporter.sendMail(newMail);
-      console.log("Email sent: " + info.response)
-   } catch(error) {
+      })
+      if (error) throw error;
+      console.log("Email sent: ", data)
+   } catch (error) {
       console.log("Error sending email: ", error);
-      throw error
+      throw error;
    }
+   // const newMail = {
+   //    from: `"Merch by Lucius" <${process.env.EMAIL}>`,
+   //    to: email,
+   //    subject: "Your verification code",
+   //    html: `<p>Your verification code is <strong>${otp}</strong>. It expires in 15 minutes.</p>`
+   // };
+   // try {
+   //    const info = await transporter.sendMail(newMail);
+   //    console.log("Email sent: " + info.response)
+   // } catch(error) {
+   //    console.log("Error sending email: ", error);
+   //    throw error
+   // }
 }
 
 module.exports = {sendVerificationEmail}

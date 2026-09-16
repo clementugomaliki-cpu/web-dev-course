3772bb0f852f619eded8fedda032c2a9981fb866
Clement Maliki <clementugomaliki@gmail.com>
2026-09-16 09:40:21 +0100

    update login route to identify account type

--- Backend/Express/MerchByLucius/routes/purchasers.js
+++ Backend/Express/MerchByLucius/routes/purchasers.js
@@ -5,6 +5,6 @@ const {purchaserSignup, verifyEmail, purchaserLogin} = require("../controllers/p
 
 router.post("/purchaser-register", purchaserSignup);
 router.post("/purchaser-verify-otp", verifyEmail);
-router.post("/login", purchaserLogin);
+//router.post("/login", purchaserLogin);
 
 module.exports = router
